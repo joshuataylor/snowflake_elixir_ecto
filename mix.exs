e36afac350e72b4a9ec8368206891ba12cc837b6
Kian Meng Ang <kianmeng.ang@gmail.com>
2020-10-25 12:48:29 +0000

    Misc doc changes (#1)
    List of changes:
    
    - Fix incorrect github repo url
    - Add ex_doc module
    - Add changelog file
    - Format markdown
    - Update links to modules
    - Badges and more badges!
    - Set readme as default html doc page

--- mix.exs
+++ mix.exs
@@ -1,31 +1,43 @@
 defmodule SnowflakeExEcto.MixProject do
   use Mix.Project
 
+  @source_url "https://github.com/joshuataylor/snowflake_elixir_ecto"
+
   def project do
     [
       app: :snowflake_elixir_ecto,
       version: "0.1.0",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
-      deps: deps(),
-      source_url: "https://github.com/joshuataylor/snowflake_elixir",
       description: "Snowflake driver written in pure Elixir, using db_connection",
+      deps: deps(),
+      docs: docs()
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
       {:snowflake_elixir, "~> 0.1.0"},
       {:dialyxir, "~> 1.0", only: [:dev], runtime: false},
-      {:mix_test_watch, "~> 1.0", only: :dev, runtime: false}
+      {:mix_test_watch, "~> 1.0", only: :dev, runtime: false},
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
+    ]
+  end
+
+  defp docs do
+    [
+      main: "readme",
+      source_url: @source_url,
+      extras: [
+        "CHANGELOG.md",
+        "README.md"
+      ]
     ]
   end
 end
